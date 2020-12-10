ec3016b8ac174250bafc542ac2df244cdcb75719
Giedo Stolk <giedo@gsit.nl>
2020-12-10 18:14:01 +0100

    Brews

---
+++ setup/brews.sh
@@ -0,0 +1,24 @@
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+#  Tools
+brew install git
+brew install --cask virtualbox
+brew install --cask vagrant
+brew install --cask vagrant-manager
+brew install --cask visual-studio-code
+brew install --cask fork
+brew install --cask remote-desktop-manager
+brew install --cask iterm2
+brew install --cask spectacle
+brew install --cask slack
+brew install --cask microsoft-teams
+brew install --cask whatsapp
+brew install --cask macpass
+brew install --cask google-chrome
+brew install --cask microsoft-office
+
+# Puppet stuff
+brew install --cask puppetlabs/puppet/puppet-bolt
+brew install --cask puppetlabs/puppet/pdk
+brew install --cask puppetlabs/puppet/wash
+brew install --cask puppetlabs/puppet/relay
+brew install --cask puppetlabs/puppet/puppet-agent-6
